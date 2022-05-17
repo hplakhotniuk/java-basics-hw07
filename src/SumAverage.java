05193fae46759dbc63ae572fac253211020dce6c
Heorhii Plakhotniuk <metalhrt7@gmail.com>
2022-05-17 23:32:57 +0300

    HW 08

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -11,8 +11,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int lowerBound, int upperBound) {
-    // TODO fill in code here using for loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int i = lowerBound; i <= upperBound; i++) {
+      sum += i;
+    }
+    return sum;
   }
 
   /**
@@ -22,8 +25,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int[] numbers) {
-    // TODO fill in code here using for each loop and replace the return statement
-    return -1;
+      int sum = 0;
+      for (int number: numbers) {
+        sum+=number;
+      }
+      return sum;
   }
 
   /**
@@ -34,8 +40,16 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int lowerBound, int upperBound) {
-    // TODO fill in code here using while loop and replace the return statement
-    return -1;
+    int counter = 0;
+    double sum = 0;
+    int i = lowerBound;
+
+    while (i <= upperBound){
+      sum+=i;
+      i++;
+      counter++;
+    }
+    return sum / counter;
   }
 
   /**
@@ -45,7 +59,17 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int[] numbers) {
-    // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
+    int counter = 0;
+    double sum = 0;
+    int i = 0;
+
+    do {
+      sum+=numbers[i];
+      i++;
+      counter++;
+    }
+    while (i < numbers.length);
+    return sum / counter;
   }
+
 }
